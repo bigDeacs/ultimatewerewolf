79333f7c8586a5fe99be964bae6922ce85e63236
bigDeacs <brentdeacon23@gmail.com>
2016-10-10 13:36:22 +1000

    afsafa

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -180,7 +180,6 @@ class GameController extends Controller {
 
 			foreach($request->input('team_list') as $key => $team)
 			{
-				dd($game->id);
 					$player = $game->players()->where('game_player.position', '=', $key)->firstOrFail();
 					$player->teams()->attach($team, ['game_id' => $game->id]);
 			}
