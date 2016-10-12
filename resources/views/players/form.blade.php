aee162127104e0f7c338fed7b98b7615d3ee457a
bigDeacs <brentdeacon23@gmail.com>
2016-10-12 16:36:29 +1000

    Add time to gamessdfaasf

--- resources/views/players/form.blade.php
+++ resources/views/players/form.blade.php
@@ -1,9 +1,6 @@
 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-<input type="hidden" name="team_id" value="null">
-<input type="hidden" name="role_id" value="null">
 <input type="hidden" name="notes" value="null">
-<input type="hidden" name="status" value="null">
 
 <div class="form-group row">
       <div class="col-xs-12">
