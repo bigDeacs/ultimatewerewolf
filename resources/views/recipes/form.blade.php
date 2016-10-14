1c254d89400e5b38c68bd3d16733756f6d6ca7b8
bigDeacs <brentdeacon23@gmail.com>
2016-10-14 16:36:32 +1000

    dfh

--- resources/views/recipes/form.blade.php
+++ resources/views/recipes/form.blade.php
@@ -1,18 +1,21 @@
 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 <div class="form-group row">
-      <div class="col-xs-12">
+      <div class="col-xs-12 col-sm-4">
           <label for="name">Name</label>
           <input type="text" name="name" id="name" class="form-control" value="{{ isset($recipe) ? $recipe->name : old('name') }}" placeholder="" required>
-          <br />
+      </div>
+      <div class="col-xs-12 col-sm-8">
           <label for="name">Description</label>
 	        {!! Form::textarea('description', null, ['class' => 'form-control textarea', 'id' => 'description', 'rows' => '10']) !!}
       </div>
+</div>
+<div class="form-group row">
       <div class="col-xs-12">
         <div class="row">
           @foreach($roles as $role)
-            <div class="col-sm-3 col-xs-12">
-              {{ $role->name }}
+            <div class="col-sm-2 col-xs-12">
+              <img src="{{ $role->image }}" class="img-responsive" alt="{{ $role->name }}" />
               <select name="role_list[{{ $role->id }}]" class="form-control">
                 @for($x = 0; $x <= $role->max; $x++)
                   @if(isset($recipe))
@@ -27,7 +30,7 @@
                 @endfor
               </select>
               <br />
-            </div>  
+            </div>
           @endforeach
         </div>
       </div>
