0c2295881dd029114d5c295dacb591a56d0a76c8
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-02-13 12:51:17 +1000

    Started Work on Ships, Need to finish Enums

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,7 @@
 	<link href="{{ asset('/css/jquery-ui.min.css') }}" rel="stylesheet">
 
 	<link href="https://cdn.datatables.net/1.10.10/css/jquery.dataTables.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link href="{{ asset('/css/select2.min.css') }}" rel="stylesheet" />
 	<link href="https://fonts.googleapis.com/css?family=Marck+Script" rel="stylesheet">
 
@@ -32,63 +32,47 @@
 			<div class="col-lg-5 col-md-5 col-sm-8 col-xs-9 bhoechie-tab-container">
 				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 bhoechie-tab-menu">
 				  <div class="list-group">
-					<a href="#" class="list-group-item active text-center">
-					  <h4 class="glyphicon glyphicon-plane"></h4><br/>Flight
-					</a>
-					<a href="#" class="list-group-item text-center">
-					  <h4 class="glyphicon glyphicon-road"></h4><br/>Train
-					</a>
-					<a href="#" class="list-group-item text-center">
-					  <h4 class="glyphicon glyphicon-home"></h4><br/>Hotel
-					</a>
-					<a href="#" class="list-group-item text-center">
-					  <h4 class="glyphicon glyphicon-cutlery"></h4><br/>Restaurant
-					</a>
-					<a href="#" class="list-group-item text-center">
-					  <h4 class="glyphicon glyphicon-credit-card"></h4><br/>Credit Card
-					</a>
+					@if(Auth::check())
+						<a href="{{ url('/players') }}" class="list-group-item active text-center">
+						  <h4 class="fa fa-users"></h4><br/>Players
+						</a>
+						@if(Auth::user()->role == 'b')
+							<a href="{{ url('/roles') }}" class="list-group-item active text-center">
+							  <h4 class="fa fa-users"></h4><br/>Roles
+							</a>
+							<a href="{{ url('/teams') }}" class="list-group-item active text-center">
+							  <h4 class="fa fa-users"></h4><br/>Teams
+							</a>
+							<a href="{{ url('/statuses') }}" class="list-group-item active text-center">
+							  <h4 class="fa fa-users"></h4><br/>Statuses
+							</a>
+							<a href="{{ url('/expansions') }}" class="list-group-item active text-center">
+							  <h4 class="fa fa-users"></h4><br/>Expansions
+							</a>
+							<a href="{{ url('/recipes') }}" class="list-group-item active text-center">
+							  <h4 class="fa fa-users"></h4><br/>Recipes
+							</a>
+							<a href="{{ url('/scenarios') }}" class="list-group-item active text-center">
+							  <h4 class="fa fa-users"></h4><br/>Scenarios
+							</a>
+						@endif
+					@endif
+					@if(Auth::guest())
+						<a href="{{ url('/auth/login') }}" class="list-group-item text-center">
+							<h4 class="fa fa-sign-in"></h4><br/>Sign In
+						</a>
+						<a href="{{ url('/auth/register') }}" class="list-group-item text-center">
+							<h4 class="fa fa-user-plus"></h4><br/>Sign Up
+						</a>
+					@else
+						<a href="{{ url('/auth/logout') }}" class="list-group-item text-center">
+							<h4 class="fa fa-sign-out"></h4><br/>Sign Out
+						</a>
+					@endif
 				  </div>
 				</div>
 				<div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 bhoechie-tab">
-					<!-- flight section -->
-					<div class="bhoechie-tab-content active">
-						<center>
-						  <h1 class="glyphicon glyphicon-plane" style="font-size:14em;color:#55518a"></h1>
-						  <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
-						  <h3 style="margin-top: 0;color:#55518a">Flight Reservation</h3>
-						</center>
-					</div>
-					<!-- train section -->
-					<div class="bhoechie-tab-content">
-						<center>
-						  <h1 class="glyphicon glyphicon-road" style="font-size:12em;color:#55518a"></h1>
-						  <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
-						  <h3 style="margin-top: 0;color:#55518a">Train Reservation</h3>
-						</center>
-					</div>
-		
-					<!-- hotel search -->
-					<div class="bhoechie-tab-content">
-						<center>
-						  <h1 class="glyphicon glyphicon-home" style="font-size:12em;color:#55518a"></h1>
-						  <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
-						  <h3 style="margin-top: 0;color:#55518a">Hotel Directory</h3>
-						</center>
-					</div>
-					<div class="bhoechie-tab-content">
-						<center>
-						  <h1 class="glyphicon glyphicon-cutlery" style="font-size:12em;color:#55518a"></h1>
-						  <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
-						  <h3 style="margin-top: 0;color:#55518a">Restaurant Diirectory</h3>
-						</center>
-					</div>
-					<div class="bhoechie-tab-content">
-						<center>
-						  <h1 class="glyphicon glyphicon-credit-card" style="font-size:12em;color:#55518a"></h1>
-						  <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
-						  <h3 style="margin-top: 0;color:#55518a">Credit Card</h3>
-						</center>
-					</div>
+					@yield('content')
 				</div>
 			</div>
 	  </div>
