d529ad56d741dad848d3b2b8d05964a36a4360eb
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-02-13 12:35:44 +1000

    Started Work on Ships, Need to finish Enums

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,167 +27,103 @@
 	<![endif]-->
 </head>
 <body>
-	<div class="row affix-row">
-		<div class="col-sm-3 col-md-2 affix-sidebar">
-			<div class="sidebar-nav">
-				<div class="navbar navbar-default" role="navigation">
-					<div class="navbar-header">
-					  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
-					  <span class="sr-only">Toggle navigation</span>
-					  <span class="icon-bar"></span>
-					  <span class="icon-bar"></span>
-					  <span class="icon-bar"></span>
-					  </button>
-					  <span class="visible-xs navbar-brand">Sidebar menu</span>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-5 col-md-5 col-sm-8 col-xs-9 bhoechie-tab-container">
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 bhoechie-tab-menu">
+				  <div class="list-group">
+					<a href="#" class="list-group-item active text-center">
+					  <h4 class="glyphicon glyphicon-plane"></h4><br/>Flight
+					</a>
+					<a href="#" class="list-group-item text-center">
+					  <h4 class="glyphicon glyphicon-road"></h4><br/>Train
+					</a>
+					<a href="#" class="list-group-item text-center">
+					  <h4 class="glyphicon glyphicon-home"></h4><br/>Hotel
+					</a>
+					<a href="#" class="list-group-item text-center">
+					  <h4 class="glyphicon glyphicon-cutlery"></h4><br/>Restaurant
+					</a>
+					<a href="#" class="list-group-item text-center">
+					  <h4 class="glyphicon glyphicon-credit-card"></h4><br/>Credit Card
+					</a>
+				  </div>
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 bhoechie-tab">
+					<!-- flight section -->
+					<div class="bhoechie-tab-content active">
+						<center>
+						  <h1 class="glyphicon glyphicon-plane" style="font-size:14em;color:#55518a"></h1>
+						  <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
+						  <h3 style="margin-top: 0;color:#55518a">Flight Reservation</h3>
+						</center>
 					</div>
-					<div class="navbar-collapse collapse sidebar-navbar-collapse">
-						<ul class="nav navbar-nav" id="sidenav01">
-							<li class="active">
-							  <a href="/" data-toggle="collapse" data-target="#toggleDemo0" data-parent="#sidenav01" class="collapsed">
-								<img src="/logo.png" class="img-responsive" />
-							  </a>
-							</li>
-							@if(Auth::check())								
-								<li><a href="{{ url('/players') }}"><h4 class="glyphicon glyphicon-lock"></h4><br/>Players</a></li>
-								@if(Auth::user()->role == 'b')
-									<li class="dropdown">
-										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Roles <span class="caret"></span></a>
-										<ul class="dropdown-menu" role="menu">
-											<li><a href="{{ url('/roles') }}">Role Cards</a></li>
-											<li><a href="{{ url('/teams') }}">Teams</a></li>
-											<li><a href="{{ url('/statuses') }}">Statuses</a></li>
-										</ul>
-									</li>
-									<li><a href="{{ url('/expansions') }}">Expansions</a></li>
-									<li><a href="{{ url('/recipes') }}">Recipes</a></li>
-									<li><a href="{{ url('/scenarios') }}">Scenarios</a></li>
-								@endif
-							@endif
-							
-							<li>
-							  <a href="#" data-toggle="collapse" data-target="#toggleDemo" data-parent="#sidenav01" class="collapsed">
-							  <h4 class="glyphicon glyphicon-cloud"></h4><br/>Flight
-							  </a>
-							  <div class="collapse" id="toggleDemo" style="height: 0px;">
-								<ul class="nav nav-list">
-								  <li><a href="#">Submenu1.1</a></li>
-								  <li><a href="#">Submenu1.2</a></li>
-								  <li><a href="#">Submenu1.3</a></li>
-								</ul>
-							  </div>
-							</li>
-							<li class="active">
-							  <a href="#" data-toggle="collapse" data-target="#toggleDemo2" data-parent="#sidenav01" class="collapsed">
-							  <span class="glyphicon glyphicon-inbox"></span> Submenu 2 <span class="caret pull-right"></span>
-							  </a>
-							  <div class="collapse" id="toggleDemo2" style="height: 0px;">
-								<ul class="nav nav-list">
-								  <li><a href="#">Submenu2.1</a></li>
-								  <li><a href="#">Submenu2.2</a></li>
-								  <li><a href="#">Submenu2.3</a></li>
-								</ul>
-							  </div>
-							</li>
-							<li><a href="#"><span class="glyphicon glyphicon-lock"></span> Normalmenu</a></li>
-							<li><a href="#"><span class="glyphicon glyphicon-calendar"></span> WithBadges <span class="badge pull-right">42</span></a></li>
-							<li><a href=""><span class="glyphicon glyphicon-cog"></span> PreferencesMenu</a></li>
-							@if(Auth::guest())
-								<li><a href="{{ url('/auth/login') }}">Login</a></li>
-								<li><a href="{{ url('/auth/register') }}">Register</a></li>
-							@else
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-							@endif
-						</ul>
-					</div><!--/.nav-collapse -->
+					<!-- train section -->
+					<div class="bhoechie-tab-content">
+						<center>
+						  <h1 class="glyphicon glyphicon-road" style="font-size:12em;color:#55518a"></h1>
+						  <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
+						  <h3 style="margin-top: 0;color:#55518a">Train Reservation</h3>
+						</center>
+					</div>
+		
+					<!-- hotel search -->
+					<div class="bhoechie-tab-content">
+						<center>
+						  <h1 class="glyphicon glyphicon-home" style="font-size:12em;color:#55518a"></h1>
+						  <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
+						  <h3 style="margin-top: 0;color:#55518a">Hotel Directory</h3>
+						</center>
+					</div>
+					<div class="bhoechie-tab-content">
+						<center>
+						  <h1 class="glyphicon glyphicon-cutlery" style="font-size:12em;color:#55518a"></h1>
+						  <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
+						  <h3 style="margin-top: 0;color:#55518a">Restaurant Diirectory</h3>
+						</center>
+					</div>
+					<div class="bhoechie-tab-content">
+						<center>
+						  <h1 class="glyphicon glyphicon-credit-card" style="font-size:12em;color:#55518a"></h1>
+						  <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
+						  <h3 style="margin-top: 0;color:#55518a">Credit Card</h3>
+						</center>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div class="col-sm-9 col-md-10 affix-content">
-			<div class="container">
-				<div class="page-header">
-					Content goes here
-				</div>
-			</div>
-		</div>
+	  </div>
 	</div>
 	
-	
-	
-	
-	
-	<nav class="navbar navbar-inverse" style="margin-bottom: 0;">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="{{ url('/') }}">Ultimate Werewolf Moderator Tools</a>
-			</div>
-
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					@if(Auth::check())
-						<li><a href="{{ url('/players') }}">Players</a></li>
-						@if(Auth::user()->role == 'b')
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Roles <span class="caret"></span></a>
-								<ul class="dropdown-menu" role="menu">
-									<li><a href="{{ url('/roles') }}">Role Cards</a></li>
-									<li><a href="{{ url('/teams') }}">Teams</a></li>
-									<li><a href="{{ url('/statuses') }}">Statuses</a></li>
-								</ul>
-							</li>
-							<li><a href="{{ url('/expansions') }}">Expansions</a></li>
-							<li><a href="{{ url('/recipes') }}">Recipes</a></li>
-							<li><a href="{{ url('/scenarios') }}">Scenarios</a></li>
-						@endif
-					@endif
-				</ul>
-
-				<ul class="nav navbar-nav navbar-right">
-					@if(Auth::guest())
-						<li><a href="{{ url('/auth/login') }}">Login</a></li>
-						<li><a href="{{ url('/auth/register') }}">Register</a></li>
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-			</div>
-		</div>
-	</nav>
-
-	@yield('content')
-
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="/js/jquery-ui.min.js"></script>
 	<script src="/js/jquery.ui.touch-punch.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-  <script src="https://cdn.datatables.net/1.10.10/js/jquery.dataTables.min.js"></script>
+	<script src="https://cdn.datatables.net/1.10.10/js/jquery.dataTables.min.js"></script>
 	<script src="/js/select2.min.js"></script>
 	<script src="/js/pick-a-color-1.2.3.min.js"></script>
 	<script src="/js/tinycolor-0.9.15.min.js"></script>
 	<script>
-      $(document).ready(function() {
-        $('.dataTable').DataTable( {
-          stateSave: true,
-          "pagingType": "full_numbers",
-					"pageLength": 500
-        });
-      });
-			$(".pick-a-color").pickAColor({
-				showSavedColors : false,
-				showBasicColors : false,
-				inlineDropdown	: true
+		$(document).ready(function() {
+			$('.dataTable').DataTable( {
+			  stateSave: true,
+			  "pagingType": "full_numbers",
+						"pageLength": 500
 			});
+			$("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
+				e.preventDefault();
+				$(this).siblings('a.active').removeClass("active");
+				$(this).addClass("active");
+				var index = $(this).index();
+				$("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+				$("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+			});
+		});
+		$(".pick-a-color").pickAColor({
+			showSavedColors : false,
+			showBasicColors : false,
+			inlineDropdown	: true
+		});
     </script>
 		@yield('scripts')
 </body>
