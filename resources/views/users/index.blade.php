f132aba1fa67b3e3aa9d830a32f30673ed0a1f13
Brent Deacon <brent.deacon@ourvacationcentre.com>
2017-05-04 09:49:24 +1000

    Add count

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -24,13 +24,15 @@
 						    <thead>
 						    	<tr>
 						    		<th>Name</th>
-						    		<th></th>
+						    		<th>Games</th>
+									<th></th>
 						    	</tr>
 						    </thead>
 						    <tbody>
 						    	@foreach($users as $user)
 									<tr>
-										<td scope="row">{{ $user->name }} <span class="badge" style="background-color: #e62929;">{{ $user->games->count() }}</span></td>
+										<td scope="row">{{ $user->name }}</td>
+										<td><span class="badge" style="background-color: #e62929;">{{ $user->games->count() }}</span></td>
 										<td><a href="/users/{{ $user->id }}" class="btn btn-warning">Edit <i class="fa fa-pencil-square-o"></i></a></td>
 									</tr>
 						    	@endforeach
