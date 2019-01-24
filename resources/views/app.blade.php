19608f538e6bcf4a128b91ad794c93ea37c732f0
Brent Deacon <brent.deacon@xtreme.com.au>
2019-01-24 16:44:56 +1000

    Test game data

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
 	<link href="https://cdn.datatables.net/1.10.10/css/jquery.dataTables.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link href="{{ asset('/css/select2.min.css') }}" rel="stylesheet" />
-	<link href="https://fonts.googleapis.com/css?family=Marck+Script" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
