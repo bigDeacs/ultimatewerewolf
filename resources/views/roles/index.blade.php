e9375388cb39c006b3a9f0d7a781042306a58300
Brent Deacon <brent.deacon@xtreme.com.au>
2019-01-29 12:08:15 +1000

    Test game data

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -65,7 +65,9 @@
                       <td>{{ $role->name }}</td>
                       <td><img src="{{ $role->image }}" class="img-responsive" /></td>
                       <td>{{ $role->impact }}</td>
-                      <td>{{ $role->team->name }}</td>
+                      <td>@if($role->team)
+                              {{ $role->team->name }}
+                      @endif</td>
                       <td>{{ $role->expansion->name }}</td>
                       <td>
                         @if($role->status)
