697a59405dd307b19860d90f5ac665ab9b120bdc
Brent Deacon <brent.deacon@xtreme.com.au>
2019-01-30 14:44:29 +1000

    Test game data

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -312,7 +312,7 @@
                       </div>                      
                     </td>
                     <td>
-                        <?php var_dump($player->teams()->where('game_id', $game->id)); ?>
+                        <?php var_dump($player->teams()->where('game_id', $game->id)->first()); ?>
                         <?php var_dump($player); ?>
                     </td>
                   </tr>
