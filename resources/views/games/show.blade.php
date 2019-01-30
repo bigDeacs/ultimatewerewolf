e6b1a996e69e67a96c99486f86828c828a67133d
Brent Deacon <brent.deacon@xtreme.com.au>
2019-01-30 14:26:28 +1000

    Test game data

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -312,7 +312,7 @@
                       </div>                      
                     </td>
                     <td>
-                        {{ $player->teams()->where('game_id', $game->id)->pivot->winner }}
+                        {{ $player->teams()->where('game_id', $game->id) }}
                     </td>
                   </tr>
                 @endforeach
